66764f3d927fdb05d32178fe8c3f1c3430d25c68
MariaTC-git <atorres@apexam.co>
2026-09-03 15:51:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.json({ limit: '50mb' }));
+
+// Configurar el motor de plantillas EJS
+app.set('view engine', 'ejs');
 
 app.post('/generar-pdf', (req, res) => {
     const data = req.body;
     
     // Puedes decidir qué plantilla usar según una variable que envíes desde n8n (ej: data.tipoPlantilla)
     const plantilla = data.tipoPlantilla === '2' ? 'certificado2' : 'certificado1';
+
+    // Cálculo automático de la fecha actual en español
+    const hoy = new Date();
+    const dia = hoy.getDate();
+    const mes = hoy.toLocaleString('es-ES', { month: 'long' });
+    const anio = hoy.getFullYear();
+    
+    data.fechaTexto = `${dia} días del mes de ${mes} de ${anio}`;
     
     res.render(plantilla, data);
 });
