ea47f48d8c2876dda1c33353801331bbcf220d6d
MariaTC-git <atorres@apexam.co>
2026-09-03 16:16:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 
 // Configurar el motor de plantillas EJS
 app.set('view engine', 'ejs');
